edcbd6885dd9c1b9b5b3552a02e8136ef10182dc
Luis Henrique <luishenriqueangelini@outlook.com>
2021-09-30 15:58:14 -0300

    Separa por erro

--- VerificaXMLOutlook/Program.cs
+++ VerificaXMLOutlook/Program.cs
@@ -15,43 +15,85 @@ namespace VerificaXMLOutlook
 {
     public class Program
     {
+        static Timer time = new Timer();
+        static string folderPendente = @"C:\Users\Desktop\TesteEmail\Pendente";
+        static string folderAprovado = @"C:\Users\Desktop\TesteEmail\Aprovado";
+        static string folderSemCertificado = @"C:\Users\Desktop\TesteEmail\SemCertificado";
+        static string folderCertificadoInvalido = @"C:\Users\Desktop\TesteEmail\CertificadoInvalido";
+
+
         public static void Main(string[] args)
         {
+            ValidarPastas();
+
+
+
             VerificationTimer(true, 5000);
+
             CreateHostBuilder(args).Build().Run();
         }
 
+        private static void ValidarPastas()
+        {
+
+            if (!Directory.Exists(folderPendente))
+                Directory.CreateDirectory(folderPendente);
+
+            if (!Directory.Exists(folderAprovado))
+                Directory.CreateDirectory(folderAprovado);
+
+            if (!Directory.Exists(folderSemCertificado))
+                Directory.CreateDirectory(folderSemCertificado);
+
+            if (!Directory.Exists(folderCertificadoInvalido))
+                Directory.CreateDirectory(folderCertificadoInvalido);
+
+        }
 
         private static void VerificationTimer(bool active, double interval)
         {
             if (active == true)
             {
-                Timer time = new Timer();
+
                 time.Interval = interval;
                 time.Elapsed += new ElapsedEventHandler(GetAttatchments);
+
                 time.Start();
-                GetAttatchments(time, null);
+                //                GetAttatchments(time, null);
             }
         }
 
 
         private static void GetAttatchments(object o, ElapsedEventArgs e)
         {
-            using (Imap imap = new Imap())
-            {
-                imap.Connect("server-IMAP");
-                imap.UseBestLogin("email-oulook", "senha");
+            time.Stop();
 
-                imap.SelectInbox();
-                //imap.Idle();
-                List<long> uids = imap.Search(Flag.Unseen);
-                foreach (long uid in uids)
+            try
+            {
+
+                using (Imap imap = new Imap())
                 {
-                    var eml = imap.GetMessageByUID(uid);
-                    IMail email = new MailBuilder().CreateFromEml(eml);
-                    SaveAttachments(email, @"C:\Source");
+                    imap.Connect("server-IMAP");
+                    imap.UseBestLogin("email-outlook", "senha");
+
+                    imap.SelectInbox();
+                    //imap.Idle();
+                    List<long> uids = imap.Search(Flag.Unseen);
+                    foreach (long uid in uids)
+                    {
+                        var eml = imap.GetMessageByUID(uid);
+                        IMail email = new MailBuilder().CreateFromEml(eml);
+
+                        SaveAttachments(email, folderPendente);
+                    }
+                    imap.Close();
                 }
-                imap.Close();
+            }
+            finally
+            {
+
+                time.Start();
+
             }
         }
 
@@ -70,8 +112,9 @@ namespace VerificaXMLOutlook
                     else
                     {
                         attachment.Save(Path.Combine(folder, attachment.SafeFileName));
-                        VerifyXML(attachment.FileName.ToString());
                     }
+
+                    VerifyXML(Path.Combine(folder, attachment.SafeFileName));
                 }
             }
         }
@@ -81,26 +124,59 @@ namespace VerificaXMLOutlook
         {
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.PreserveWhitespace = true;
+
             xmlDoc.Load(xmlName);
-            SignedXml signedXml = new SignedXml(xmlDoc);
-            XmlNodeList nodeList = xmlDoc.GetElementsByTagName("Signature");
-            XmlNodeList certificates = xmlDoc.GetElementsByTagName("X509Certificate");
-            X509Certificate2 dcert2 = new X509Certificate2(Convert.FromBase64String(certificates[0].InnerText));
-            foreach (XmlElement element in nodeList)
+
+            try
             {
-                signedXml.LoadXml(element);
-                bool passes = signedXml.CheckSignature(dcert2, true);
-                string sourceFile = @"C:\Source\" + xmlName;
-                if (passes == true)
+                SignedXml signedXml = new SignedXml(xmlDoc);
+                XmlNodeList nodeList = xmlDoc.GetElementsByTagName("Signature");
+                XmlNodeList certificates = xmlDoc.GetElementsByTagName("X509Certificate");
+
+                string sourceFile = Path.Combine(folderPendente, xmlName);
+
+                if (certificates.Count > 0)
                 {
-                    string destinationFile = @"C:\Users\documents\Aprovado\" + xmlName;
-                    File.Move(sourceFile, destinationFile);
+                    try
+                    {
+                        X509Certificate2 dcert2 = new X509Certificate2(Convert.FromBase64String(certificates[0].InnerText));
+                        foreach (XmlElement element in nodeList)
+                        {
+                            signedXml.LoadXml(element);
+                            bool passes = signedXml.CheckSignature(dcert2, true);
+
+                            if (passes == true)
+                            {
+                                string destinationFile = Path.Combine(folderAprovado, Path.GetFileName(xmlName));
+                                File.Move(sourceFile, destinationFile);
+                            }
+
+                            else
+                            {
+                                string destinationFile = Path.Combine(folderCertificadoInvalido, Path.GetFileName(xmlName));
+                                File.Move(sourceFile, destinationFile);
+                            }
+
+                        }
+                    }
+                    catch
+                    {
+                        string destinationFile = Path.Combine(folderCertificadoInvalido, Path.GetFileName(xmlName));
+                        File.Move(sourceFile, destinationFile);
+                    }
                 }
                 else
                 {
-                    string destinationFile = @"C:\Users\documents\Reprovado\" + xmlName;
+                    string destinationFile = Path.Combine(folderSemCertificado, Path.GetFileName(xmlName));
                     File.Move(sourceFile, destinationFile);
                 }
+
+            }
+            finally
+            {
+                xmlDoc = null;
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
             }
         }
 
